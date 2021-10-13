bac6219426cdf6d748ea6665b0b7b3159ef78d46
Lachlan-Denham <denham.lachlan32@gmail.com>
2021-10-13 14:50:05 +1100

    added basic scaffold to appfile for all sections to be used

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,1 +1,28 @@
 # To be filled by students
+import streamlit as st
+
+def launchApp():
+    
+
+    header = st.container()
+    studentA = st.container()
+    studentB = st.container()
+    studentC = st.container()
+    studentD = st.container()
+
+    with header:
+        st.title('DSP Assignment 3')
+
+    with studentA:
+        st.header('Overall Information')
+
+    with studentB:
+        st.header('Information on each numeric column')
+
+    with studentC:
+        st.header('Information on each text column')
+
+    with studentD:
+        st.header('Information on each datetime column')
+    
+    return
