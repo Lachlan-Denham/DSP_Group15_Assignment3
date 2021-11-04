b5b0b06f9ca081571e065a2fe74f759770e3b283
DStockdale1 <79729012+DStockdale1@users.noreply.github.com>
2021-11-04 23:28:25 +1100

    Added additional comments for various functions

--- src/text.py
+++ src/text.py
@@ -12,61 +12,63 @@ class TextColumn:
 
   def get_name(self):
     """
-    Return name of selected column
+    Return the name of the selected column
     """
     return self.col_name
 
   def get_unique(self):
     """
-    Return number of unique values for selected column
+    Return the number of  the unique values for a selected column
     """
     return self.serie.nunique()
 
   def get_missing(self):
     """
-    Return number of missing values for selected column
+    Return the number of missing values for a selected column
     """
     return self.serie.isna().sum()
 
   def get_empty(self):
     """
-    Return number of rows with empty string for selected column
+    Return the number of rows with an empty string for a selected column
+    Uses isnull() but might be better to sum over cells with length == 0 
     """
     return self.serie.isnull().sum()
 
   def get_whitespace(self):
     """
-    Return number of rows with only whitespaces for selected column
+    Return the number of rows with only whitespaces for selected column
     """
     return self.serie.str.isspace().sum()
 
   def get_lowercase(self):
     """
-    Return number of rows with only lower case characters for selected column
+    Return the number of rows with only lower case characters for selected column
     """
     return self.serie.str.islower().sum()
 
   def get_uppercase(self):
     """
-    Return number of rows with only upper case characters for selected column
+    Return the number of rows with only upper case characters for selected column
     """
     return self.serie.str.isupper().sum()
 
   def get_alphabet(self):
     """
-    Return number of rows with only alphabet characters for selected column
+    Return the number of rows with only alphabet characters for selected column
     """
     return self.serie.str.isalpha().sum()
 
   def get_digit(self):
     """
-    Return number of rows with only numbers as characters for selected column
+    Return the number of rows with only numbers as characters for selected column
     """
     return self.serie.str.isdigit().sum()
 
   def get_mode(self):
     """
     Return the mode value for selected column
+    If multiple modes occur, first value alphabetically is shown in app
     """
     return self.serie.mode()
 
@@ -74,10 +76,13 @@ class TextColumn:
   def get_barchart(self, col_name):
     """
     Return the generated bar chart for selected column  - bar chart is sorted
+
+    Bar chart generated using altair
+    Index is reset after counting in descending order
+    Column names changed to required convention
     """
 
-    df = self.serie.value_counts().reset_index() # works
-    #df.columns = ['Value', 'Number of Occurances']
+    df = self.serie.value_counts().reset_index()
     col_name = str(col_name)
     df.columns = [col_name, 'Count of Records']
 
@@ -93,6 +98,7 @@ class TextColumn:
   def get_frequent(self):
     """
     Return the Pandas dataframe containing the occurrences and percentage of the top 20 most frequent values
+    Table is descending by %
     """
     n = 20
     df = self.serie.value_counts().reset_index()
